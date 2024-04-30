1a5c5e68a01164db9acc6b33c06a647c4b283b56
Sebastian Villada Jaramillo <sebastian.villada@tmsolutions.com.co>
2024-04-29 19:00:35 -0500

    [CONFIG] Se realizan las operaciones con metodos

--- app/src/main/java/com/example/sumarrestarnumeros/MainActivity.java
+++ app/src/main/java/com/example/sumarrestarnumeros/MainActivity.java
@@ -13,12 +13,13 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements
         View.OnClickListener {
     EditText et1, et2;
     TextView tv1;
-    Button btnsuma, btnresta, btnmultiplicar;
+    Button btnsuma, btnresta, btnmultiplicar, btndividir;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,33 +27,62 @@ public class MainActivity extends AppCompatActivity implements
         et1=(EditText)findViewById(R.id.txtnumero1);
         et2=(EditText)findViewById(R.id.txtnumero2);
         tv1=(TextView) findViewById(R.id.lblresultado);
+
         btnsuma=(Button) findViewById(R.id.btnsuma);
         btnsuma.setOnClickListener(this);
 
         btnresta=(Button) findViewById(R.id.btnrestar);
-        btnresta.setOnClickListener(this);
+        btnresta.setOnClickListener(this::resta);
 
         btnmultiplicar=(Button) findViewById(R.id.btnmultiplicar);
-        btnmultiplicar.setOnClickListener(this);
+        btnmultiplicar.setOnClickListener(this::multiplicar);
+
+        btndividir=(Button) findViewById(R.id.btndividir);
+        btndividir.setOnClickListener(this::dividir);
     }
     @Override
     public void onClick(View v) {
         et1=(EditText)findViewById(R.id.txtnumero1);
         et2=(EditText)findViewById(R.id.txtnumero2);
-        float n1,n2,res, resta;
+        float n1,n2,res;
         n1=Float.parseFloat(et1.getText().toString());
         n2=Float.parseFloat(et2.getText().toString());
+        res=n1+n2;
+        tv1.setText("El resultado es: " + res);
 
-        if(v.getId() == R.id.btnsuma){
-            res=n1+n2;
-            tv1.setText("El resultado es: " + res);
-        } else if (v.getId() == R.id.btnrestar){
-            res=n1-n2;
-            tv1.setText("El resultado es: " + res);
-        } else if (v.getId() == R.id.btnmultiplicar){
-            res=n1*n2;
-            tv1.setText("El resultado es: " + res);
-        }
-        //Toast.makeText(getApplicationContext(), "Ha Presionado el boton",Toast.LENGTH_SHORT).show();
+        Toast.makeText(getApplicationContext(), "Ha Presionado el boton",Toast.LENGTH_SHORT).show();
+    }
+    public void resta(View view){
+        et1=(EditText)findViewById(R.id.txtnumero1);
+        et2=(EditText)findViewById(R.id.txtnumero2);
+        float n1,n2,res;
+        n1=Float.parseFloat(et1.getText().toString());
+        n2=Float.parseFloat(et2.getText().toString());
+        res=n1-n2;
+        tv1.setText("El resultado es: " + res);
+
+        Toast.makeText(getApplicationContext(), "Ha Presionado el boton",Toast.LENGTH_SHORT).show();
+    }
+    public void multiplicar(View view){
+        et1=(EditText)findViewById(R.id.txtnumero1);
+        et2=(EditText)findViewById(R.id.txtnumero2);
+        float n1,n2,res;
+        n1=Float.parseFloat(et1.getText().toString());
+        n2=Float.parseFloat(et2.getText().toString());
+        res=n1*n2;
+        tv1.setText("El resultado es: " + res);
+
+        Toast.makeText(getApplicationContext(), "Ha Presionado el boton",Toast.LENGTH_SHORT).show();
+    }
+    public void dividir(View view){
+        et1=(EditText)findViewById(R.id.txtnumero1);
+        et2=(EditText)findViewById(R.id.txtnumero2);
+        float n1,n2,res;
+        n1=Float.parseFloat(et1.getText().toString());
+        n2=Float.parseFloat(et2.getText().toString());
+        res=n1/n2;
+        tv1.setText("El resultado es: " + res);
+
+        Toast.makeText(getApplicationContext(), "Ha Presionado el boton",Toast.LENGTH_SHORT).show();
     }
 }
